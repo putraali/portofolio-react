364c2710d4c4162f38bd529042c2c480136bb91b
putraali <k9724aliputra@gmail.com>
2025-07-10 16:07:46 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
