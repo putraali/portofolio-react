01f37b4da127b90050f65aa02b267ea5488398bd
putraali <k9724aliputra@gmail.com>
2025-07-10 15:09:10 +0700

    delete

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/portofolio-react/', // ⬅️ PENTING! Harus sesuai dengan nama repo GitHub kamu
-  plugins: [react()],
-})
