d8340e4fb04b09959a54d23eb054c1d334b67171
putraali <k9724aliputra@gmail.com>
2025-07-10 15:40:57 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/portofolio-react/', // ← ini yang sangat penting!
-  plugins: [react()],
-})
+	base: "/portofolio-react/", // ← sesuai nama repo kamu
+	plugins: [react()],
+});
